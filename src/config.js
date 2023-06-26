300bc1ed29d34ee7e5d7c2e68dc754f8104db115
Yonashenok <yonashenok24@gmail.com>
2023-06-26 16:13:24 +0300

    feat: add all the constant values

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const API_URL_CREATE = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+export const API_URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/JHdzJDECyCIhp5rZcsYP/scores/';
+export const ID = 'JHdzJDECyCIhp5rZcsYP';
